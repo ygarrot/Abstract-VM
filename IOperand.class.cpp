1bece6677d5bdd1d8a2049a916a1a4ca4bc91853
yoan garrot <garrot.yoan1@gmail.com>
2019-03-16 17:19:28 +0100

    operand class

---
+++ IOperand.class.cpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   IOperand.class.cpp                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ygarrot <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/03/16 16:48:24 by ygarrot           #+#    #+#             */
+/*   Updated: 2019/03/16 16:48:29 by ygarrot          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+class IOperand {
+	public:
+		virtual int getPrecision( void ) const = 0; // Precision of the type of the instance
+		virtual eOperandType getType( void ) const = 0; // Type of the instance
+		virtual IOperand const * operator+( IOperand const & rhs ) const = 0; // Sum
+		virtual IOperand const * operator-( IOperand const & rhs ) const = 0; // Difference
+		virtual IOperand const * operator*( IOperand const & rhs ) const = 0; // Product
+		virtual IOperand const * operator/( IOperand const & rhs ) const = 0; // Quotient
+		virtual IOperand const * operator%( IOperand const & rhs ) const = 0; // Modulo
+		virtual std::string const & toString( void ) const = 0; // String representation of the instance
+		virtual ~IOperand( void ) {}
+};
