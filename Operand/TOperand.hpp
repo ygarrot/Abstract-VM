efec85a774c82ff3e2c589c2f6a18781818b3b67
yoan garrot <garrot.yoan1@gmail.com>
2019-03-23 14:35:19 +0100

    will handle exceptions later;; template partially done

--- Operand/TOperand.hpp
+++ Operand/TOperand.hpp
@@ -6,14 +6,13 @@
 /*   By: ygarrot <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/21 15:09:14 by ygarrot           #+#    #+#             */
-/*   Updated: 2019/03/23 13:17:35 by ygarrot          ###   ########.fr       */
+/*   Updated: 2019/03/23 14:33:39 by ygarrot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 #ifndef ABSTRACT_VM_TOPERAND_HPP
 #define ABSTRACT_VM_TOPERAND_HPP
-
 
 #include <cfenv>
 #include <iostream>
@@ -24,45 +23,57 @@
 template<typename T>
 class TOperand: public IOperand {
 	public:
-		virtual int getPrecision( void ) const {return (1);}; // Precision of the type of the instance
-		virtual eOperandType getType( void ) const { return INT8;}; // Type of the instance
-		/* eOperandType getType( void ){}; // Type of the instance */
-		virtual void check_exceptions(TOperand const & rhs) const;
-		/* virtual void check_underflow(IOperand const & rhs); */
-		/* virtual void check_overflow(TOperand const & rhs); */
-		virtual IOperand const * operator+( IOperand const & rhs ) const;
-		/* virtual IOperand const * operator+( IOperand const & rhs ) const;//{(void)rhs;return new TOperand<T>();}; // Sum */
-		virtual IOperand const * operator-( IOperand const & rhs ) const;
-		virtual IOperand const * operator*( IOperand const & rhs ) const{(void)rhs;return new TOperand<T>();}; // Product
-		virtual IOperand const * operator/( IOperand const & rhs ) const{(void)rhs;return new TOperand<T>();}; // Quotient
-		virtual IOperand const * operator%( IOperand const & rhs ) const{(void)rhs;return new TOperand<T>();}; // Modulo
-		virtual std::string const & toString( void ) const {return this->_str;}; // String representation of the instance
-		IOperand &operator=(IOperand const & src){(void)src; return *this;};
+		IOperand &operator=(IOperand const & src);
 		TOperand(){};
 		TOperand(std::string){};
 		TOperand(T type){this->_n = type;};
 		TOperand(TOperand const & src){(void)src;};
 		~TOperand(void){};
 		int		get_value(void) const {return this->_n;};
+		virtual	int getPrecision( void ) const;
+		virtual	eOperandType getType( void ) const;
+		virtual void	check_exceptions(TOperand const & rhs) const;
+		virtual	IOperand const * operator+( IOperand const & rhs ) const;
+		virtual	IOperand const * operator-( IOperand const & rhs ) const;
+		virtual	IOperand const * operator*( IOperand const & rhs ) const{(void)rhs;return new TOperand<T>();};
+		virtual	IOperand const * operator/( IOperand const & rhs ) const{(void)rhs;return new TOperand<T>();};
+		virtual	IOperand const * operator%( IOperand const & rhs ) const{(void)rhs;return new TOperand<T>();};
+		virtual	std::string const & toString( void ) const;
 	private:
-		std::string _str;
 		T _n;
+		eOperandType _type;
 };
 
 
-bool mulOverflow(long long a, long long b) 
-{ 
-	long long result = a * b; 
-	if (a == 0 || b == 0)  
-		;
-	if (a == result / b) 
-		;
-	else
-		;
-	return true;
+template<typename T>
+IOperand &TOperand<T>::operator=(IOperand const & src)
+{
+	TOperand<T> const & tmp = reinterpret_cast< const TOperand<T>& >(src);
+	if (tmp == this)
+		return *this;
+	_n = tmp._n;
+	_type = tmp._type;
 }
 
-	template<typename T>
+template<typename T>
+int TOperand<T>::getPrecision( void ) const
+{
+	return (this->_n);
+}
+
+template<typename T>
+std::string const & TOperand<T>::toString( void ) const
+{
+	return (std::to_string(this->_n));
+}
+
+template<typename T>
+eOperandType TOperand<T>::getType( void ) const
+{
+	return (this->_type);
+}
+
+template<typename T>
 int subOvf(T a, T b) 
 { 
 	std::feclearexcept(FE_ALL_EXCEPT);
@@ -79,9 +90,8 @@ int subOvf(T a, T b)
 	template<typename T>
 int addOvf(T a, T b) 
 { 
-	if ( (a < 0.0) == (b < 0.0)
-			&& ( b ) > std::numeric_limits<double>::max() - std::abs( a ) ) {
-		//  Addition would overflow...
+	if ((a < 0.0) == (b < 0.0)
+			&& std::abs(b) > std::numeric_limits<double>::max() - std::abs(a)) {
 	}
 	return 1;
 } 
@@ -92,7 +102,6 @@ void TOperand<T>::check_exceptions(TOperand const & rhs) const
 	if (!std::isnormal(this->_n) || !std::isnormal(rhs._n))
 		;
 	subOvf<T>(this->_n, rhs._n);
-	(void)rhs;
 }
 
 	template<typename T>
@@ -106,18 +115,18 @@ template <typename T>
 IOperand const * TOperand<T>::operator-( IOperand const & rhs ) const
 {
 	TOperand<T> const & tmp = reinterpret_cast< const TOperand<T>& >(rhs);
-	(void)rhs;
 	check_exceptions(tmp);
-	return (new TOperand<T>(this->_n - reinterpret_cast< const TOperand<T>& >(rhs)._n));
+	return (new TOperand<T>(this->_n - tmp._n));
 }
 
 template <typename T>
 IOperand const * TOperand<T>::operator+( IOperand const & rhs ) const
 {
 	TOperand<T> const & tmp = reinterpret_cast< const TOperand<T>& >(rhs);
-	(void)rhs;
 	check_exceptions(tmp);
-	return (new TOperand<T>(this->_n + tmp._n));
+	return new TOperand<T>(this->_n + tmp._n);
 }
+
+/* ... */
 
 #endif
