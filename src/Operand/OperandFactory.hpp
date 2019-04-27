2aeacae184c3ff4ba94d1d3dde1f3731a8938917
yoan garrot <garrot.yoan1@gmail.com>
2019-04-27 12:45:33 +0200

    clean repo

--- src/Operand/OperandFactory.hpp
+++ src/Operand/OperandFactory.hpp
@@ -6,7 +6,7 @@
 /*   By: ygarrot <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/24 14:30:23 by ygarrot           #+#    #+#             */
-/*   Updated: 2019/04/27 11:58:30 by ygarrot          ###   ########.fr       */
+/*   Updated: 2019/04/27 12:43:46 by ygarrot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
 
 #include "IOperand.hpp"
 #include "boost/variant.hpp"
-# define abstr_variant boost::variant<int8_t, int16_t, int32_t, float, double> 
 #include "TOperand.hpp"
 #include <map>
 #include <regex>
